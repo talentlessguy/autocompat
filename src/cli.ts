9d74aea391ed7311e9df46c95c982e042df9f8ac
v1rtl <hi@v1rtl.site>
2024-11-24 17:51:25 +0200

    init

---
+++ src/cli.ts
@@ -0,0 +1,37 @@
+import { CLI } from 'spektr'
+import {
+	crawlDependencies,
+	findClosestPkgJsonPath,
+	getPackageFiles,
+} from './utils'
+
+const cli = new CLI({ name: 'autocompat' })
+
+cli.command(
+	'crawl',
+	(_, { limit }) => {
+		const packageJsonPath = findClosestPkgJsonPath(process.cwd())
+		const crawlLimit = limit ? Number.parseInt(limit) : Number.POSITIVE_INFINITY
+		if (!packageJsonPath) {
+			console.error(`No closest package.json found from ${process.cwd()}`)
+			process.exit(1)
+		}
+		const dependencyMetadatas = crawlDependencies(packageJsonPath, crawlLimit)
+
+		for (const dep of dependencyMetadatas) {
+			getPackageFiles(dep.pkgDir)
+		}
+	},
+	{
+		options: [
+			{
+				name: 'limit',
+				type: 'string',
+				description: 'Limit the number of dependencies to crawl',
+				required: false,
+			},
+		] as const,
+	},
+)
+
+cli.handle(process.argv.slice(2))
---
+++ src/utils.ts
@@ -0,0 +1,118 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
+type PackageManifest = {
+	name: string
+	version: string
+	dependencies?: Record<string, string>
+	devDependencies?: Record<string, string>
+	main?: string
+	module?: string
+	files?: string[]
+}
+
+type DependencyMetadata = {
+	pkgDir: string
+	pkgGraphPath: string[]
+	pkgVersion: string
+	pkgJson: Partial<PackageManifest>
+}
+
+export function crawlDependencies(
+	pkgJsonPath: string,
+	limit: number,
+): DependencyMetadata[] {
+	const metadatas: DependencyMetadata[] = []
+
+	crawl(path.dirname(pkgJsonPath), [], true)
+
+	if (limit != null && metadatas.length >= limit) {
+		return metadatas.slice(0, limit)
+	}
+
+	for (const metadata of metadatas) {
+		crawl(metadata.pkgDir, metadata.pkgGraphPath)
+
+		if (limit != null && metadatas.length >= limit) {
+			break
+		}
+	}
+
+	return metadatas
+
+	function crawl(pkgDir: string, parentDepNames: string[], isRoot = false) {
+		const pkgJsonContent = fs.readFileSync(
+			path.join(pkgDir, 'package.json'),
+			'utf8',
+		)
+		const pkgJson = JSON.parse(pkgJsonContent)
+		const pkgDependencies = Object.keys(pkgJson.dependencies || {})
+
+		if (isRoot) {
+			pkgDependencies.push(...Object.keys(pkgJson.devDependencies || {}))
+		}
+
+		for (const depName of pkgDependencies) {
+			// Prevent dep loop
+			if (parentDepNames.includes(depName)) continue
+
+			const depPkgJsonPath = findPkgJsonPath(depName, pkgDir)
+			if (!depPkgJsonPath) continue
+
+			const depPkgJson = JSON.parse(fs.readFileSync(depPkgJsonPath, 'utf8'))
+
+			metadatas.push({
+				pkgDir: path.dirname(depPkgJsonPath),
+				pkgGraphPath: parentDepNames.concat(depPkgJson.name),
+				pkgVersion: depPkgJson.version,
+				pkgJson: depPkgJson,
+			})
+
+			if (limit != null && metadatas.length >= limit) {
+				break
+			}
+		}
+	}
+}
+
+export function findClosestPkgJsonPath(dir: string) {
+	while (dir) {
+		const pkg = path.join(dir, 'package.json')
+		try {
+			if (fs.existsSync(pkg)) {
+				return pkg
+			}
+		} catch {}
+		const nextDir = path.dirname(dir)
+		if (nextDir === dir) break
+		dir = nextDir
+	}
+	return undefined
+}
+
+export function findPkgJsonPath(pkgName: string, basedir: string) {
+	while (basedir) {
+		const pkg = path.join(basedir, 'node_modules', pkgName, 'package.json')
+		try {
+			if (fs.existsSync(pkg)) {
+				return fs.realpathSync(pkg)
+			}
+		} catch {}
+		const nextBasedir = path.dirname(basedir)
+		if (nextBasedir === basedir) break
+		basedir = nextBasedir
+	}
+	return undefined
+}
+
+const validExtensions = ['js', 'd.ts', 'mjs', 'cjs', 'json']
+
+export function getPackageFiles(dir: string) {
+	const files = fs.readdirSync(dir, { recursive: true })
+
+	const filteredFiles = files.filter((file) =>
+		validExtensions.some((ext) => (file as string).endsWith(ext)),
+	)
+
+	return filteredFiles
+}
